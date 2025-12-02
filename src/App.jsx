478af7319c07e0865cde424f64b615f93861c45d
Taman Hassan <tamanhassan1@gmail.com>
2025-12-02 21:52:45 +0100

    changes made to bitcoin.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Bitcoin from "../components/Bitcoin.jsx";
+
+function App() {
+    return (
+      <div className="App">
+        <Bitcoin/>
+
+      </div>
+    );
+}
+
+export default App;
